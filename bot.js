5b1b28be52109473daa649a29a8bd680398cecde
fhetheteh45 <45371878+fhetheteh45@users.noreply.github.com>
2018-11-27 00:24:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "2";
+var prefix = "play2";
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : EX Clan`);
